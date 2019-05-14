b4691da9b0d4e60c4a1388d08bdf131f964a813c
Elizarlo <eluisramos0909@gmail.com>
2019-05-14 11:06:48 -0500

    vivero

---
+++ vivero.php
@@ -0,0 +1,25 @@
+<?php
+ // Credenciales
+ $dbhost = "localhost";
+ $dbuser = "Karen";
+ $dbpass = "password";
+ $dbname = "datos";
+ //Conexion con la base de datos
+ $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+?>
+
+<?php
+ //https://amestris.me/Vivero/vivero.php?Temperatura=50&Humedad=44
+ //Importamos la configuracion
+ // Leemos los valores que nos llegan por GET
+ $Temperatura = mysqli_real_escape_string($con, $_GET['Temperatura']);
+ $Humedad = mysqli_real_escape_string($con, $_GET['Humedad']);
+ // EInsertamos los valores en la tabla
+ $query = "INSERT INTO Valores(temperatura, humedad) VALUES('$Temperatura','$Humedad')";
+ // Ejecutamos la instruccion
+ mysqli_query($con, $query);
+ mysqli_close($con);
+ echo "Pagina para subir los datos<br />";
+ echo "<br />Temperatura = $Temperatura ºC<br />";
+ echo "<br />Humedad = $Humedad %<br />";
+?>
