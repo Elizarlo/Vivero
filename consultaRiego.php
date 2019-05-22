ca56624d5b1ce2e3bbdcce45d3fc69d3012d3a44
Elizarlo <eluisramos0909@gmail.com>
2019-05-21 20:46:59 -0500

    riego

---
+++ consultaRiego.php
@@ -0,0 +1,30 @@
+<?php
+ // Credenciales
+ $dbhost = "localhost";
+ $dbuser = "Karen";
+ $dbpass = "password";
+ $dbname = "datos";
+ //Conexion con la base de datos
+ $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+?>
+
+<?php
+ 
+ // Consultar los datos
+ $query = "SELECT * FROM riego order by dia";
+ // Ejecutamos la instruccion
+$result =  mysqli_query($con, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+	 echo "Dia = $row['dia'] ,";
+	 echo "Hora = $row['hora'] ,";
+	echo "Duracion = $row['duracion'] <br />";	 
+    }
+} else {
+    echo "0 results";
+}
+
+ mysqli_close($con);
+?>
