4eaa7b0c0f40568b43de65531cd492572412c1be
Elizarlo <eluisramos0909@gmail.com>
2019-05-21 20:55:55 -0500

    riego

--- consultaRiego.php
+++ consultaRiego.php
@@ -14,10 +14,11 @@
  $query = "SELECT * FROM riego order by dia";
  // Ejecutamos la instruccion
 $result =  mysqli_query($con, $query);
-
+	echo "Bienvenido";
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
+	echo "entre";
 	 echo "Dia = $row['dia'] ,";
 	 echo "Hora = $row['hora'] ,";
 	echo "Duracion = $row['duracion'] <br />";	 
