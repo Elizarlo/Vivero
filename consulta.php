b492a33dd140d321fb75cd5fbc9f289ccf177b74
Elizarlo <eluisramos0909@gmail.com>
2019-05-21 21:21:37 -0500

    riego

--- consulta.php
+++ consulta.php
@@ -18,9 +18,9 @@ $result =  mysqli_query($con, $query);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-	 echo "Tiempo = $row['tiempo'] <br />";
-	 echo "Temperatura = $row['temperatura'] <br />";
-	echo "Humedad = $row['humedad'] <br />";	 
+	 echo "Tiempo = " .$row['tiempo'] ."<br />";
+	 echo "Temperatura = " .$row['temperatura'] ."<br />";
+	echo "Humedad = " .$row['humedad'] ."<br />";	 
     }
 } else {
     echo "0 results";
--- consultaRiego.php
+++ consultaRiego.php
@@ -14,14 +14,12 @@
  $query = "SELECT * FROM riego order by dia";
  // Ejecutamos la instruccion
 $result =  mysqli_query($con, $query);
-	echo "Bienvenido";
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-	echo "entre";
-	 echo "Dia = $row['dia'] ,";
-	 echo "Hora = $row['hora'] ,";
-	echo "Duracion = $row['duracion'] <br />";	 
+	 echo "Dia = ".$row['dia'] .",";
+	 echo "Hora = ".$row['hora'] .",";
+	echo "Duracion = ". $row['duracion']." <br />";	 
     }
 } else {
     echo "0 results";
